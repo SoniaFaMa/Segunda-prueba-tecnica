0c17f6c30f72a6d4ad23fe42d506ada1590f6096
SoniaFaMa <101564312+SoniaFaMa@users.noreply.github.com>
2024-09-11 20:20:25 +0200

    Components

--- src/Components/LogoutButton/LogoutButton.js
+++ src/Components/LogoutButton/LogoutButton.js
@@ -2,6 +2,8 @@ import './logoutButton.css'
 
 export default function LogotButtom({handleLogout,translations,language}){
 
+  console.log("hola")
+
     return(
   <button className="logout-button" onClick={handleLogout}>{translations[language].logout}</button>
     )
